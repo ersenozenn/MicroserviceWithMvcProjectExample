feb395d6d36966f19ab14b02d5b9a93aa48c999f
Ersen Ozen <ersenozen@outlook.com>
2022-07-13 22:16:58 +0300

    CompanyDTO Created.

---
+++ Company.Domain/DTOS/CompanyDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Domain.DTOS
+{
+    public class CompanyDTO
+    {
+        public string CompanyName { get; set; }
+        public string CompanyType { get; set; }
+        public string TaxAdmin { get; set; }
+        public string TaxNumber { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }        
+    }
+}
