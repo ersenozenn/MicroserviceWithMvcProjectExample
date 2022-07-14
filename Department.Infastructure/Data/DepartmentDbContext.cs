c787cadc9f7dbee855bfa0edcb94e106fafe3f1f
Ersen Ozen <ersenozen@outlook.com>
2022-07-14 23:49:00 +0300

    Department database created.

---
+++ Department.Infastructure/Data/DepartmentDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Department.Domain.DTOS;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Department.Infastructure.Data
+{
+    public class DepartmentDbContext : DbContext
+    {
+        public DepartmentDbContext(DbContextOptions<DepartmentDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<DepartmentDTO> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-5LLUNOS\\SQLEXPRESS;Initial Catalog=DepartmentDB;Integrated Security=true;");
+            }
+        }
+    }
+}
